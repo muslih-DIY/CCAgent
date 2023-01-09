4b38535858c04e5f9219b9cb8d758cbb28b306a5
muslihk_diy <muslihk00@gmail.com>
2023-01-09 08:32:05 +0530

    initial commit

---
+++ CCAgent/ConcreteStates.py
@@ -0,0 +1,21 @@
+
+from . Agent import AgentState
+
+class UnAvailable(AgentState):
+    pass
+
+class Ready(AgentState):
+    pass
+
+class Paused(AgentState):
+    pass
+
+class Calling(AgentState):
+    pass
+
+class Hold(AgentState):
+    pass
+
+class Ringing(AgentState):
+    pass
+
